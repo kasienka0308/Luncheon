d37a2aeef52447f1247fa27fe79971c9fd0b543d
kasienka0308 <katarzynamakarowicz@gmail.com>
2019-10-02 21:56:34 +0200

    update registration

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -1,23 +1,42 @@
-import React from "react";
-import './Registration.css';
+import React, { useState } from "react";
+import "./Registration.css";
 function Registration() {
+  const [name, setName] = useState("");
+  const [shift, setShift] = useState("");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+  function handleShiftChange(event) {
+    setShift(event.target.value);
+  }
   return (
     <section>
       <form>
         <div className="form-element">
           <label for="name">Name</label>
-          <input type="text" name="name" id="name"></input>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleNameChange}
+          ></input>
         </div>
         <div className="form-element">
-            <label for="shift">Choose your shift</label>
-            <select id="shift">
-                <option value="">Please choose your shift</option>
-                <option value="A">Shift A (8:00 - 16:00)</option>
-                <option value="B">Shift B (9:00 - 17:00)</option>
-                <option value="C">Shift C (10:00 - 18:00)</option>
-            </select>
+          <label for="shift">Choose your shift</label>
+          <select id="shift" onChange={handleShiftChange}>
+            <option value="">Please choose your shift</option>
+            <option value="A">Shift A (8:00 - 16:00)</option>
+            <option value="B">Shift B (9:00 - 17:00)</option>
+            <option value="C">Shift C (10:00 - 18:00)</option>
+          </select>
         </div>
       </form>
+
+      {name.length < 5 ? <b className="error-message">too short</b> : null}
+      {name.length > 5 ? <button>Continue</button> : null}
+      {/* <b>your name is:{name}</b>
+      <b>your shift is:{shift}</b> */}
     </section>
   );
 }
