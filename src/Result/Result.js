62b90ff50635bef9636d3458b5e3d785a11e760b
kasienka0308 <katarzynamakarowicz@gmail.com>
2019-10-03 22:16:56 +0200

    basic  result page, shuffle

--- src/Result/Result.js
+++ src/Result/Result.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+
+// durstenfeld shuffle
+function shuffleArray(array) {
+  const randomOrder = [...array];
+  for (let i = randomOrder.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [randomOrder[i], randomOrder[j]] = [randomOrder[j], randomOrder[i]];
+  }
+  return randomOrder;
+}
 
 function Result() {
-    return <h1>Result not ready</h1>
+  // mock fake people's names for the shifts
+  const shiftA = shuffleArray(["Scarlet", "Emma", "Michal"]);
+  const shiftB = shuffleArray(["Ella", "Bartek", "Avery"]);
+  const shiftC = shuffleArray(["Zoey", "Wyatt", "Jack"]);
+  // adding all the arrays together keeping A, B, C preference
+  const lunchOrder = shiftA.concat(shiftB).concat(shiftC);
+
+  return (
+    <ol>
+      {lunchOrder.map(name => {
+        return <li>{name}</li>;
+      })}
+    </ol>
+  );
 }
 
 export default Result;
